473d391f5e567d669dca7f40bf199f28dad77c24
0x0u <m0zurillex@gmail.com>
2020-05-14 23:35:32 +0900

    feat: バックグラウンドでタブの消滅を監視してtinderUrlを開いているタブが無くなったらボタンステータスを全て非アクティブにする

--- src/background.js
+++ src/background.js
@@ -1,5 +1,13 @@
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        console.log(request.msg);
-        sendResponse({msg: request.msg})
+// タブの消滅を監視してtinderUrlを開いているタブが無くなったらボタンステータスを全て非アクティブにする
+
+const tinderUrl = "https://tinder.com/app/recs";
+
+chrome.tabs.onRemoved.addListener(() => {
+    chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT},(tabs) => {
+        const tabList = tabs.map(tab => tab.url);
+        if (!tabList.includes(tinderUrl)) {
+            localStorage.likeBtnStatus = "inactive";
+            localStorage.nopeBtnStatus = "inactive";
+        }
+    });
 })
